0dc759073062fd620ded4b57fecaf2558d0cacef
HP657 <junlee5070@gmail.com>
2024-07-28 21:15:59 +0900

    라우팅 설정 변경

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Post from "./Component/Post";
 import SignUpPage from "./Component/SignUpPage";
 import SignInPage from "./Component/SignInPage";
-import Main from './Component/Main';
+import Main from "./Component/Main";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Post />} />
-          <Route path="signup" element={<SignUpPage/>} />
-          <Route path="signin" element={<SignInPage/>} />
-          <Route path="main" element={<Main/>} />
+          <Route path="/main" element={<Post />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
