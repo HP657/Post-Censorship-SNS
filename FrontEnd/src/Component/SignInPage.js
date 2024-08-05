91c226b640929930f0ff6102280916f54d173bfb
hp657 <junlee5070@gmail.com>
2024-08-05 12:42:11 +0900

    약간 수정

--- FrontEnd/src/Component/SignInPage.js
+++ FrontEnd/src/Component/SignInPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../Css/Form.css";
+import "../Css/Form.css"; // Import your Form.css here
 import { useNavigate } from "react-router-dom";
 
 const SignInPage = () => {
@@ -40,28 +40,36 @@ const SignInPage = () => {
   };
 
   return (
-    <div className="auth-container">
-      <form onSubmit={handleSubmit} className="auth-form">
-        <h2>로그인</h2>
-        {error && <p className="error-message">{error}</p>}
-        <input
-          type="email"
-          placeholder="이메일"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="비밀번호"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "로그인 중..." : "로그인"}
-        </button>
-      </form>
+    <div className="full">
+      <div className="auth-container">
+        <form onSubmit={handleSubmit} className="auth-form">
+          <h2>로그인</h2>
+          {error && <p className="error-message">{error}</p>}
+          <input
+            type="email"
+            placeholder="이메일"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button className="login-button" type="submit" disabled={loading}>
+            {loading ? "로그인 중..." : "로그인"}
+          </button>
+          <button className="signup-button" onClick={() => navigate("/signup")}>
+            회원가입
+          </button>
+          <button className="home-button" onClick={() => navigate("/")}>
+            홈
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
