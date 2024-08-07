ab137cbcd5936920ba5fe51d4384f0e78fa25907
hp657 <junlee5070@gmail.com>
2024-08-07 15:24:30 +0900

    AI 통신으로 검증 후 디비 수정

--- BackEnd/src/main/java/com/SWAI/HP657/service/PostService.java
+++ BackEnd/src/main/java/com/SWAI/HP657/service/PostService.java
@@ -14,8 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PostService {
@@ -29,6 +27,10 @@ public class PostService {
     @Autowired
     private ImageService imageService;
 
+    @Autowired
+    private FlaskService flaskService;
+
+    @Transactional
     public Response<String> postAdd(PostUploadDto postUploadDto, HttpServletRequest request) throws IOException {
         Long userId = (Long) request.getSession().getAttribute("userId");
 
@@ -38,16 +40,28 @@ public class PostService {
         Posts post = new Posts();
         post.setUser(user);
         post.setPostText(postUploadDto.getContent());
-        post.setShare(false); // 기본값을 false로 설정
+        post.setShare(false);
 
         MultipartFile postImg = postUploadDto.getPostImg();
         if (postImg != null && !postImg.isEmpty()) {
             String postImgUrl = imageService.uploadImage(postImg, "posts");
             post.setPostImgUrl(postImgUrl);
+
+            post = postRepository.save(post);
+            Long postId = post.getPostId();
+
+            boolean isConfident = flaskService.verifyImageWithFlask(postImgUrl);
+            System.out.println(isConfident);
+            if (isConfident) {
+                post.setShare(true);
+                postRepository.save(post);
+                return new Response<>("게시물 업로드 성공적 (이미지 검증 성공)", HttpStatus.OK);
+            } else {
+                return new Response<>("게시물 업로드 성공적 (이미지 검증 실패)", HttpStatus.OK);
+            }
+        } else {
+            postRepository.save(post);
+            return new Response<>("게시물 업로드 성공적 (이미지 없음)", HttpStatus.OK);
         }
-
-        postRepository.save(post);
-        return new Response<>("게시물 업로드 성공적", HttpStatus.OK);
     }
-
 }
