98f0e8ed956b4d5ad9b3b749f3c906770bfc7ad8
HP657 <junlee5070@gmail.com>
2024-08-08 21:36:55 +0900

    RestTemplate 추가

--- BackEnd/src/main/java/com/SWAI/HP657/config/AppConfig.java
+++ BackEnd/src/main/java/com/SWAI/HP657/config/AppConfig.java
@@ -1,9 +1,15 @@
 package com.SWAI.HP657.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @PropertySource("classpath:application.secret.properties")
 public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
