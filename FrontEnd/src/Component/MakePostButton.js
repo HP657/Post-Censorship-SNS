f9ae68e54cfd3e8cea5175205eedff25e2a07d87
hp657 <junlee5070@gmail.com>
2024-08-09 13:12:08 +0900

    게시물 작성 페이지 이동 버튼

--- FrontEnd/src/Component/MakePostButton.js
+++ FrontEnd/src/Component/MakePostButton.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../Css/Button.css";
+import { useNavigate } from "react-router-dom";
 
 const MakePostButton = () => {
   const handleClick = () => {
     console.log("포스트 작성하기 버튼 클릭됨");
+    navigate('/mapo')
   };
+
+  const navigate = useNavigate();
 
   return (
     <div className="postbutton-container">
