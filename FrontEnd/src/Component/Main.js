075fda999eff3115d3b2edb09e32da54773289f5
hp657 <junlee5070@gmail.com>
2024-08-09 13:11:05 +0900

    게시물 보임

--- FrontEnd/src/Component/Main.js
+++ FrontEnd/src/Component/Main.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../Css/Main.css";
 import MakePostButton from "./MakePostButton";
+import ViewContent from "./ViewContent";
 import { useNavigate } from "react-router-dom";
 
 axios.defaults.withCredentials = true;
 
 const Main = () => {
   const [userInfo, setUserInfo] = useState(null);
+  const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
 
   const handleClick = (label) => {
@@ -23,11 +25,21 @@ const Main = () => {
     }
   };
 
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/post/a/view");
+      setPosts(response.data.data);
+    } catch (error) {
+      console.error("Error fetching posts: ", error);
+    }
+  };
+  
+
   const logout = () => {
     try {
       axios.post("http://localhost:8080/api/auth/logout");
-      setUserInfo(null); // Reset userInfo on logout
-      navigate("/signin"); // Navigate to sign-in page on logout
+      setUserInfo(null);
+      navigate("/signin");
     } catch (error) {
       console.error("Error logging out: ", error);
     }
@@ -35,12 +47,14 @@ const Main = () => {
 
   useEffect(() => {
     fetchUserInfo();
+    fetchPosts();
   }, []);
 
   return (
     <div className="container">
       <header className="header">
         <div className="logo">Logo</div>
+          <MakePostButton />
         <div className="loginarea">
           {userInfo ? (
             <div>
@@ -48,16 +62,15 @@ const Main = () => {
               <button className="logout-button" onClick={logout}>로그아웃</button>
             </div>
           ) : (
-            <div>
-              <button className="signup-button" onClick={() => {navigate('/signup');}}>회원가입</button>
-              <button className="signin-button" onClick={() => {navigate('/signin');}}>로그인</button>
+            <div className="button-container">
+              <button className="signup-button" onClick={() => { navigate('/signup'); }}>회원가입</button>
+              <button className="signin-button" onClick={() => { navigate('/signin'); }}>로그인</button>
             </div>
           )}
         </div>
       </header>
-      <MakePostButton />
       <main className="content">
-        <div>Content Area</div>
+        <ViewContent posts={posts} />
       </main>
       <footer className="footer">
         <div className="footerItem" onClick={() => handleClick("Home")}>
