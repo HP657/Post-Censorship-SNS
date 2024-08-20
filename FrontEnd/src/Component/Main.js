bd33846e5dee864033ca4b70d3de8d042b8cf837
hp657 <junlee5070@gmail.com>
2024-08-20 19:17:12 +0900

    기본 프로필 사진 변수 추가

--- FrontEnd/src/Component/Main.js
+++ FrontEnd/src/Component/Main.js
@@ -8,18 +8,26 @@ axios.defaults.withCredentials = true;
 
 const Main = () => {
   const [userInfo, setUserInfo] = useState(null);
+  const [UserProfileImg, setUserProfileImg] = useState("imgs/person.png")
   const navigate = useNavigate();
 
   const handleClick = (label) => {
-    if (label === "Home") {
-      navigate("/");
+    console.log(`${label} clicked`);
+    switch (label) {
+      case "Home":
+        navigate("/");
+        break;
+      case "Profile":
+        navigate("/mypage");
+        break;
     }
-  };
+};
 
   const fetchUserInfo = async () => {
     try {
       const response = await axios.get("http://localhost:8080/api/auth/info");
       setUserInfo(response.data.data);
+      setUserProfileImg(userInfo.profileImgUrl)
     } catch (error) {
       console.error("Error fetching user info: ", error);
     }
@@ -42,36 +50,18 @@ const Main = () => {
   return (
     <div className="container">
       <header className="header">
-        <a href="/">
-          <div className="logo">Logo</div>
-        </a>
-        <MakePostButton />
+        <div className="logo" onClick={() => handleClick("Home")}>Logo</div>
+          <MakePostButton />
         <div className="loginarea">
           {userInfo ? (
             <div>
               <p>Welcome, {userInfo.username}</p>
-              <button className="logout-button" onClick={logout}>
-                로그아웃
-              </button>
+              <button className="logout-button" onClick={logout}>로그아웃</button>
             </div>
           ) : (
             <div className="button-container">
-              <button
-                className="signup-button"
-                onClick={() => {
-                  navigate("/signup");
-                }}
-              >
-                회원가입
-              </button>
-              <button
-                className="signin-button"
-                onClick={() => {
-                  navigate("/signin");
-                }}
-              >
-                로그인
-              </button>
+              <button className="signup-button" onClick={() => { navigate('/signup'); }}>회원가입</button>
+              <button className="signin-button" onClick={() => { navigate('/signin'); }}>로그인</button>
             </div>
           )}
         </div>
@@ -88,12 +78,8 @@ const Main = () => {
           <img src="/imgs/setting.png" alt="Settings" className="img" />
           <button className="footer-button">Settings</button>
         </div>
-        <div className="footerItem" onClick={() => handleClick("Menu")}>
-          <img
-            src={userInfo ? userInfo.profileImgUrl : "imgs/person.png"}
-            alt="Profile"
-            className="img"
-          />
+        <div className="footerItem" onClick={() => handleClick("Profile")}>
+          <img src={UserProfileImg} alt="Profile" className="img" />
           <button className="footer-button">Profile</button>
         </div>
       </footer>
