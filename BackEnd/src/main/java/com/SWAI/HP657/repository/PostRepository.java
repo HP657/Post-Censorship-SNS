5634b2f7618b127f51cebbfe6fa52568c39e11b6
hp657 <junlee5070@gmail.com>
2024-08-20 19:15:13 +0900

    유저의 게시물 가져오는 JPA와 postId로 상세 페이지 위한 JPA 추가

--- BackEnd/src/main/java/com/SWAI/HP657/repository/PostRepository.java
+++ BackEnd/src/main/java/com/SWAI/HP657/repository/PostRepository.java
@@ -1,17 +1,16 @@
 package com.SWAI.HP657.repository;
 
 import com.SWAI.HP657.entity.Posts;
-import com.SWAI.HP657.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Posts, Long> {
-    Optional<Posts> findByPostId(Long postId);
-    List<Posts> findByUser_UserId(Long userId);
     List<Posts> findByShareTrueOrderByPostIdDesc();
     List<Posts> findByShareFalseOrderByPostIdDesc();
     List<Posts> findAllByOrderByPostIdDesc();
+    Optional<Posts> findByPostId(Long postId);
+    List<Posts> findByUser_UserIdOrderByPostIdDesc(Long userId);
 }
 
